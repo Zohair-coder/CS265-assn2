2f6f2cd766bd26ef1d67474ac3a9ad5824149e52
Zohair-coder <zohair.ul.hasan@gmail.com>
2020-11-18 21:14:41 +0500

    Created prototype of Aldo, Bar, Calma and Dol funcitons

--- fsm.c
+++ fsm.c
@@ -3,11 +3,16 @@
 #include <string.h>
 
 char *inputString(FILE *fp, size_t *len, size_t size);
+int isValidAldo(char *msg, int len);
+int isValidBar(char *msg, int len);
+int isValidCalma(char *msg, int len);
+int isValidDol(char *msg, int len);
 
 int main(int argc, char **argv)
 {
     FILE *fin = stdin;
     size_t *msg_len = (size_t *)malloc(sizeof(size_t));
+    int is_valid_msg = 0;
 
     if (argc > 1)
     {
@@ -22,9 +27,24 @@ int main(int argc, char **argv)
     char *current_message = inputString(fin, msg_len, 10);
     while (*current_message != '\0')
     {
-        printf("Current Message: %s\nLength: %i\n", current_message, *msg_len);
-        printf("First character of current message: %c\n", *current_message);
-        printf("Second character of current message: %c\n\n", *(current_message + 1));
+        // printf("Current Message: %s\nLength: %i\n", current_message, *msg_len);
+        // printf("First character of current message: %c\n", *current_message);
+        // printf("Second character of current message: %c\n\n", *(current_message + 1));
+        is_valid_msg = 0;
+
+        is_valid_msg = isValidAldo(current_message, *msg_len);
+        is_valid_msg = isValidBar(current_message, *msg_len);
+        is_valid_msg = isValidCalma(current_message, *msg_len);
+        is_valid_msg = isValidDol(current_message, *msg_len);
+
+        if (is_valid_msg)
+        {
+            printf("%s OK\n", current_message);
+        }
+        else
+        {
+            printf("%s FAIL\n", current_message);
+        }
 
         current_message = inputString(fin, msg_len, 10);
     }
@@ -53,4 +73,25 @@ char *inputString(FILE *fp, size_t *len, size_t size)
     str[*len++] = '\0';
 
     return realloc(str, sizeof(char) * (*len));
+}
+
+int isValidAldo(char *msg, int len)
+{
+
+    return 0;
+}
+
+int isValidBar(char *msg, int len)
+{
+    return 0;
+}
+
+int isValidCalma(char *msg, int len)
+{
+    return 0;
+}
+
+int isValidDol(char *msg, int len)
+{
+    return 0;
 }
