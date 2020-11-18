792c0f1d3a3d3ea1e9ee8baba6eacc1d663662ab
Zohair-coder <zohair.ul.hasan@gmail.com>
2020-11-18 18:07:17 +0500

    Implemented new technique to get input

--- fsm.c
+++ fsm.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+char *inputString(FILE *fp, size_t size);
+
 int main(int argc, char **argv)
 {
     FILE *fin = stdin;
@@ -16,6 +18,35 @@ int main(int argc, char **argv)
         }
     }
 
-    int c = fgetc(fin);
-    printf("First character: %c", c);
+    char *current_message = inputString(fin, 10);
+    while (*current_message != '\0')
+    {
+        printf("%s\n", current_message);
+        current_message = inputString(fin, 10);
+    }
+}
+
+char *inputString(FILE *fp, size_t size)
+{
+    //The size is extended by the input with the value of the provisional
+    char *str;
+    int ch;
+    size_t len = 0;
+    str = realloc(NULL, sizeof(char) * size); //size is start size
+    if (!str)
+        return str;
+
+    while (EOF != (ch = fgetc(fp)) && ch != '\n')
+    {
+        str[len++] = ch;
+        if (len == size)
+        {
+            str = realloc(str, sizeof(char) * (size += 16));
+            if (!str)
+                return str;
+        }
+    }
+    str[len++] = '\0';
+
+    return realloc(str, sizeof(char) * len);
 }
