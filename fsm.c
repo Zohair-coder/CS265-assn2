895c4620f9c513869be7d039a58cc8ee7946c448
Zohair-coder <Zohair.ul.hasan@gmail.com>
2020-11-19 00:24:51 +0500

    Initialized current_message pointer to NULL

--- fsm.c
+++ fsm.c
@@ -17,7 +17,7 @@ int main(int argc, char **argv)
     int is_valid_bar = 0;
     int is_valid_calma = 0;
     int is_valid_dol = 0;
-    size_t buffer = 16;
+    size_t buffer = 0;
 
     if (argc > 1)
     {
@@ -29,8 +29,9 @@ int main(int argc, char **argv)
         }
     }
 
-    char *current_message;
+    char *current_message = NULL;
     size_t msg_len = 0;
+
     while ((msg_len = getline(&current_message, &buffer, fin)) != -1)
     {
         current_message[msg_len - 1] = '\0';
